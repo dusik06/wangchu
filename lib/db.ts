cb1da6645d4bdb8e3e8c45cb0b61bc6b758298af
entlr06 <cksqls06@gmail.com>
2026-06-14 05:26:38 +0900

    fix db env

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,14 @@
 import mysql from "mysql2/promise";
 
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "zxcv5623",
-  database: "wangchu",
-  charset: "utf8",
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "zxcv5623",
+  database: process.env.DB_NAME || "wangchu",
+  charset: "utf8mb4",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 export default db;
