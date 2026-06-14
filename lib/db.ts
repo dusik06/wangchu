12bf884ff6741e702b270582b119cb77a2c882a3
entlr06 <cksqls06@gmail.com>
2026-06-14 22:22:13 +0900

    update homepage

--- lib/db.ts
+++ lib/db.ts
@@ -5,7 +5,7 @@ const db = mysql.createPool({
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "zxcv5623",
   database: process.env.DB_NAME || "wangchu",
-  charset: "UTF8_GENERAL_CI",
+  charset: "utf8",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
